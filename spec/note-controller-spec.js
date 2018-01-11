69ae6d63a424d3ca81935e6eb1a9bd4fda0f4b2e
charles <charlesemery15@gmail.com>
2018-01-11 17:26:28 +0000

    doubled for controller tests

--- spec/note-controller-spec.js
+++ spec/note-controller-spec.js
@@ -1,5 +1,31 @@
 function test_twelve(){
   var notelist = new NoteList();
-  notelist.createAndAddNote('first note')
-  notelist.createAndAddNote('second note')
-}
+  var controller = new Controller(notelist);
+  assert.isTrue(controller instanceof Controller, 'Can instantiate a controller ')
+};
+
+test_twelve();
+
+window.onload = function(){
+  function test_thirteen(){
+
+    var DoubleNoteList = function(){
+    };
+
+    var DoubleView = function(){
+      DoubleView.prototype.returnHTML = function(){
+        return '<ul><li><div>Favourite Drink: Spiced rum and Ginger beer</div></li></ul>';
+      };
+    };
+
+    var notelist = new DoubleNoteList();
+    var view = new DoubleView();
+    var controller = new Controller(notelist, view);
+    var html = controller.view.returnHTML();
+    console.log(html)
+    controller.getHTML('app')
+    console.log(controller.getHTML('app'))
+    assert.isTrue(document.getElementById('app').innerHTML === html, 'InnerHTML should contain a note list with one note ')
+  };
+  test_thirteen();
+};
--- src/note-controller.js
+++ src/note-controller.js
@@ -1,17 +1,20 @@
+(function(exports){
+  function Controller(notelist = new NoteList(), view = new View(notelist)){
+    this.notelist = notelist;
+    this.view = view;
+  };
+
+  Controller.prototype.getHTML = function(elementID){
+    console.log(document.getElementsByTagName('body'));
+    console.log(document.getElementById(elementID));
+    document.getElementById(elementID).innerHTML = this.view.returnHTML();
+  };
+
+  exports.Controller = Controller;
+})(this);
+
 window.onload = function(){
-  (function(exports){
-    function Controller(notelist = new NoteList()){
-      this.notelist = notelist
-      this.notelist.createAndAddNote('Favourite Drink: Spiced rum and Gingerbeer')
-      this.view = new View(this.notelist)
-
-      Controller.prototype.getHTML = function(){
-        document.getElementById('app').innerHTML = this.view.returnHTML();
-      };
-    };
-    exports.Controller = Controller;
-  })(this);
-
-  controller = new Controller()
-  controller.getHTML()
+  controller = new Controller();
+  controller.notelist.createAndAddNote('Favourite Drink: Spiced rum and Ginger beer');
+  controller.getHTML('app');
 };
